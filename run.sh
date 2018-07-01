40035411930d87193f0d77e500980decf601827b
J. Pylypiw <jochen@pylypiw.com>
2018-07-01 10:05:57 +0200

    Upvloop and run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+# Use #1 to run OWAPI using uvloop for better performance
+# Use #2 to run OWAPI without uvloop for compatibility reasons
+
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-cd /opt/OWAPI
+#1
+PYTHONPATH=. pipenv run asphalt run -l uvloop config.yml
 
-# PYTHONPATH=. asphalt run -l uvloop config.yml
-PYTHONPATH=. pipenv run asphalt run config.yml
+#2
+#PYTHONPATH=. pipenv run asphalt run config.yml
 
 exit 0
